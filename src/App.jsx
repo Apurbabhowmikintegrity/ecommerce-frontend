dc2aef9996dc0f51644d0b4d6428b6da14c43980
Apurba Bhowmik <Apurba.Bhowmik@inwebinfo.com>
2025-02-13 19:16:42 +0530

    push code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './component/Header'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './page/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path={'/'} element={<Home />}/>
+  </Routes>
+  </BrowserRouter>
    </>
   )
 }
